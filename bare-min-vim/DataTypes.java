54443323dd52b84b9922e8722513b87cd81db472
k-nautresurrect <kushignite@gmail.com>
2022-05-14 19:38:59 +0530

    Added files for migration

---
+++ bare-min-vim/DataTypes.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+
+public class DataTypes {
+	public static void main(String[] args){
+		byte by = 12;
+		short sh = 18;
+		int inT = 78;
+		long lonG = 87;
+		char ch = 452; // only supported character by system
+
+		System.out.println(by + " " 
+				+ sh 
+				+ " " 
+				+ inT 
+				+ " " 
+				+ lonG 
+				+ " " +
+				ch);
+	}
+}
